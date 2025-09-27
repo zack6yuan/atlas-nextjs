7fcb8cd826a9c8f701fefcf13750df2a49e2a0fd
zack6yuan <zack6yuan@gmail.com>
2025-09-27 15:31:37 -0500

    updates.

--- app/ui/topics/[id]/page.tsx
+++ app/ui/topics/[id]/page.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-
-export default function Page({params}: { params: { id: string } }) {
-    return <div className="">Topics Page: {params.id}</div>
+export default async function Page({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+  return <div>Topic Page: {id}</div>;
 }
