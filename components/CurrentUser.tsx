1d112fb46a5d8d41e6db04c6327669c838323586
zack6yuan <zack6yuan@gmail.com>
2025-10-09 11:15:25 -0500

    test user.

--- components/CurrentUser.tsx
+++ components/CurrentUser.tsx
@@ -12,7 +12,7 @@ export default async function CurrentUser() {
     <div>
       <div className="flex h-[48px] w-full grow items-center justify-center gap-2 bg-gray-50 p-3 text-sm font-medium md:flex-none md:justify-start md:p-2 md:px-3">
         <img src={session.user.image ? session.user.image : "../assets/user.png"} alt="" className="w-8 rounded-full" />
-        <div className="hidden md:block">{session.user.name}</div>
+        <div className="hidden md:block">{session.user.name ? session.user.name : "Test User"}</div>
       </div>
     </div>
   );
