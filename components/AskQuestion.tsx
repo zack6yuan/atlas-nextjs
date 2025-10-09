7f0edb630821b5161f861bcbf58b7c29c0e38037
zack6yuan <zack6yuan@gmail.com>
2025-10-08 22:28:18 -0500

    hover properties of the hover and answer buttons.

--- components/AskQuestion.tsx
+++ components/AskQuestion.tsx
@@ -10,7 +10,7 @@ export function AskQuestion({ topic }: { topic: string }) {
         placeholder="Ask a question"
         className="mt-1 block w-full rounded-md border border-atlas-white-300 bg-inherit py-3 pl-3 pr-28 text-lg text-gray-900 placeholder-gray-400 focus:outline-hidden focus:ring-3 focus:ring-atlas-teal"
       />
-      <button className="absolute right-2 top-2 flex h-10 w-24 items-center justify-center rounded-md border bg-secondary px-4 text-lg text-white hover:text-atlas-white-300 focus:bg-secondary">
+      <button className="absolute right-2 top-2 flex h-10 w-24 items-center justify-center rounded-md border bg-secondary px-4 text-lg text-white hover:bg-white hover:text-inherit hover:border-blue-950 hover:border-3 focus:bg-secondary">
         Ask
       </button>
     </form>
--- components/GiveAnswer.tsx
+++ components/GiveAnswer.tsx
@@ -16,7 +16,7 @@ export function GiveAnswer({ topic }: GiveAnswerProps) {
         placeholder="Answer question"
         className="mt-1 block w-full rounded-md border border-atlas-white-300 bg-inherit py-3 pl-3 pr-28 text-lg text-gray-900 placeholder-gray-400 focus:outline-hidden focus:ring-3 focus:ring-atlas-teal"
       />
-      <button className="absolute right-2 top-2 flex h-10 w-24 items-center justify-center rounded-md border bg-secondary px-4 text-lg text-white focus:bg-secondary">
+      <button className="absolute right-2 top-2 flex h-10 w-24 items-center justify-center rounded-md border bg-secondary px-4 text-lg text-white hover:bg-white hover:text-inherit hover:border-blue-950 hover:border-3 focus:bg-secondary">
         Answer
       </button>
     </form>
