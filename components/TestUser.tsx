b4c7c2a62108b49eb076719b33daae5e4c441ee4
zack6yuan <zack6yuan@gmail.com>
2025-10-09 11:43:24 -0500

    updating the user logged in.

--- components/TestUser.tsx
+++ components/TestUser.tsx
@@ -1,10 +1,11 @@
+import userImage from "../assets/user.png"
+
 export default async function TestUser() {
-  const userImage = "../assets/user.png";
   return (
     <div>
       <div className="flex h-[48px] w-full grow items-center justify-center gap-2 bg-gray-50 p-3 text-sm font-medium md:flex-none md:justify-start md:p-2 md:px-3">
-        <img src={userImage} alt="" className="w-8 rounded-full" />
-        <div className="hidden md:block">{"Test User"}</div>
+        <img src={userImage.src} alt="" className="w-8 rounded-full" />
+        <p>Test User</p>
       </div>
     </div>
   );
