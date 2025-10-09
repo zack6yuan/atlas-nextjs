7172109cf94b9d5ee39bd2ea1fd78f3a76f6d951
zack6yuan <zack6yuan@gmail.com>
2025-10-09 10:39:02 -0500

    update and redeploy.

--- components/CurrentUser.tsx
+++ components/CurrentUser.tsx
@@ -5,13 +5,14 @@ export default async function CurrentUser() {
 
   if (!session?.user) return null
   if (!session.user.image) return null
+  if (!session.user.name) return null
 
 
   return (
     <div>
       <div className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium md:flex-none md:justify-start md:p-2 md:px-3">
         <img src={session.user.image} alt="" className="w-8" />
-        <div className="hidden md:block">Test User</div>
+        <div className="hidden md:block">{session.user.name}</div>
       </div>
     </div>
   );
