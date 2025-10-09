d5511bc128fcb276bd37c514fb3963cdecac649c
zack6yuan <zack6yuan@gmail.com>
2025-10-09 10:32:56 -0500

    updated the session image.

--- components/CurrentUser.tsx
+++ components/CurrentUser.tsx
@@ -1,11 +1,16 @@
-import user from "../assets/user.png";
-import Link from "next/link";
+import { auth } from "../auth";
 
-export default function CurrentUser() {
+export default async function CurrentUser() {
+  const session  = await auth();
+
+  if (!session?.user) return null
+  if (!session.user.image) return null
+
+
   return (
     <div>
       <div className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium md:flex-none md:justify-start md:p-2 md:px-3">
-        <img src={user.src} alt="" className="w-8" />
+        <img src={session.user.image} alt="" className="w-8" />
         <div className="hidden md:block">Test User</div>
       </div>
     </div>
